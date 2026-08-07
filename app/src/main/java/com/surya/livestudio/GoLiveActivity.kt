0983c1bc4063ce76a48030070335977cb38542c4
officialsuryaverse-create <official.suryaverse@gmail.com>
2026-08-07 14:59:18 +0530

    Phase 2 - Go Live screen

---
+++ app/src/main/java/com/surya/livestudio/GoLiveActivity.kt
@@ -0,0 +1,11 @@
+package com.surya.livestudio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GoLiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_go_live)
+    }
+}
--- app/src/main/java/com/surya/livestudio/MainActivity.kt
+++ app/src/main/java/com/surya/livestudio/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.surya.livestudio
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGoLive = findViewById<Button>(R.id.btnGoLive)
+
+        btnGoLive.setOnClickListener {
+            startActivity(Intent(this, GoLiveActivity::class.java))
+        }
     }
 }
